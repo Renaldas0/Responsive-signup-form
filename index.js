2986e9ffa4854ed9c54c27e23dcf41d6cec31d0b
Renaldas Bendikas <renaldasb1999@gmail.com>
2023-11-30 17:35:36 +0000

    add functionality to form

---
+++ index.js
@@ -0,0 +1,44 @@
+// required email field lights up on incorrect input
+// display success message on form submit
+// dismiss message to bring back to main form page
+
+const form = document.querySelector('form');
+const emailInput = document.getElementById('email-input');
+const errorMsg = document.getElementById('error-msg');
+const dismissBtn = document.querySelector('#dismiss-btn');
+const submitBtn = document.querySelector('#submit-btn');
+const successContainer = document.querySelector('#success-container');
+const newsletterContainer = document.querySelector('#newsletter');
+
+const errorBgColor = 'rgba(255, 0, 0, 0.22)';
+const errorColor = 'rgba(255, 0, 0, 0.78)';
+
+const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    if (emailInput.value == null || emailInput.value === "") {
+        console.log('email field is empty');
+        errorMsg.textContent = 'Please enter your email address';
+        emailInput.style.background = errorBgColor;
+    } else if (!emailRegex.test(emailInput.value)) {
+        console.log('email is not valid');
+        errorMsg.textContent = 'Please enter a valid email address';
+        emailInput.style.background = errorBgColor;
+        emailInput.style.color = errorColor;
+    } else {
+        console.log('email is valid');
+        errorMsg.textContent = '';
+        emailInput.style.background = '';
+        emailInput.style.color = 'var(--Dark-Slate-Grey)';
+        newsletterContainer.classList.add('hidden');
+        successContainer.classList.remove('hidden');
+        emailInput.value = '';
+    }
+});
+
+dismissBtn.addEventListener('click', () => {
+    newsletterContainer.classList.remove('hidden');
+    successContainer.classList.add('hidden');
+})
--- index.ts
+++
@@ -1,3 +0,0 @@
-// required email field lights up on incorrect input
-// display success message on form submit
-// dismiss message to bring back to main form page
